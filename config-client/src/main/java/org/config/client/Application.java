357a6e523a2e08683d63a57790c19b408988ed52
huangchong <784047104@qq.com>
2017-06-17 22:52:23 +0800

    分布式配置中心，使用git做外部配置

--- config-client/src/main/java/org/config/client/Application.java
+++ config-client/src/main/java/org/config/client/Application.java
@@ -1,12 +1,10 @@
-package org.config.server;
+package org.config.client;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableDiscoveryClient
-@EnableConfigServer
 @SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
